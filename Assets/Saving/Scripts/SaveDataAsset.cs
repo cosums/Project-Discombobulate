9a5348b12076e50a36dc9458545430fee3e8e31a
cosums <114266944+cosums@users.noreply.github.com>
2026-09-24 14:42:41 -0500

    adding some save flags

--- Assets/Saving/Scripts/SaveDataAsset.cs
+++ Assets/Saving/Scripts/SaveDataAsset.cs
@@ -8,6 +8,26 @@ using UnityEngine;
 public class SaveDataAsset : ScriptableObject
 {
     public string uuid;
+    // key flags
+    public bool hasLibraryKey;
+    public bool hasOfficeKey;
+    public bool hasMasterBedroomKey;
+    public bool hasMalloryBedroomKey;
+    public bool hasSecretRoomKey;
+
+    // item flags
+    public bool HasMotherDeadNotice;
+    public bool HasFatherJournal;
+    public bool Hs1873Newspaper;
+    public bool HasRosethroneLedger;
+    public bool HsCreditorTelegram;
+    public bool HasMallorysJournal;
+    public bool HasTrainTicket;
+    public bool HasPackedTrunk;
+    public bool HasNewspaperDeaths;
+    public bool HasMurderWeapon;
+    public bool HasSuicideWeapon;
+    public bool HasFathersNote;
 
     public SaveData ToSaveData()
     {
